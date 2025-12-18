e7a6e962106df32e7cbec6545f966768bd186c10
itsara <khajer@gmail.com>
2025-12-18 16:33:42 +0700

    add info log

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,21 @@ use tokio::sync::Mutex;
 
 mod shared;
 use shared::ShareMemory;
+use tracing::info;
+use tracing_subscriber::fmt;
+
+fn setup_logging() {
+    fmt()
+        .with_target(false)
+        .with_max_level(tracing::Level::INFO)
+        .init();
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    setup_logging();
     let host = "127.0.0.1:6411";
-    println!("Starting server at {}", host);
+    info!("Starting server at {}", host);
     let listener = TcpListener::bind(host).await?;
     let shared_memory = Arc::new(Mutex::new(ShareMemory::new()));
 
@@ -17,7 +27,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let shared_memory_clone = Arc::clone(&shared_memory);
 
         let (mut socket, addr) = listener.accept().await?;
-        println!("Accepted connection from {}", addr);
+        info!("Accepted connection from {}", addr);
 
         tokio::spawn(async move {
             let mut sm = shared_memory_clone.lock().await;
